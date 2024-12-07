83f9e0ce476962041eead142cebbbfa4496557ea
Gabriela Gonzalez <gabrielagonzalezpalacio@gmail.com>
2024-12-07 20:38:56 -0300

    cambios en views, models y urls

--- proyecto_django/App/models.py
+++ proyecto_django/App/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class socios(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    dni = models.CharField(max_length=10, unique=True)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+
+class actividades(models.Model):
+    nombre = models.CharField(max_length=30)
+    horario = models.TimeField()
+    profesor = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+
+class sucursales(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+
--- proyecto_django/App/views.py
+++ proyecto_django/App/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def inicio(request):
+    return HttpResponse("Vista inicio")
+
+def socios(request):
+    return HttpResponse("vista socios")
+
+def actividades(request):
+    return HttpResponse("vista actividades")
+
+def sucursales(request):
+    return HttpResponse("vista sucursales")
