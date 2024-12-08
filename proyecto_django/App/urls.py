09d891b9edb8ef31977ab7e6de7d06916f447cac
Gabriela Gonzalez <gabrielagonzalezpalacio@gmail.com>
2024-12-08 18:04:38 -0300

    navegar entre templates

--- proyecto_django/App/urls.py
+++ proyecto_django/App/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from App import views
 
 urlpatterns = [
-    path("", views.inicio),
+    path("", views.inicio, name = "inicio"),
     path("socios/", views.socios, name = "socios"),
     path("actividades/", views.actividades, name = "actividades"),
     path("sucursales/", views.sucursales, name = "sucursales")
