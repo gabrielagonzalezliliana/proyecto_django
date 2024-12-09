b3c0ccd03d5522f221399ba794dbe2815fee31a6
Gabriela Gonzalez <gabrielagonzalezpalacio@gmail.com>
2024-12-08 23:39:08 -0300

    creacion de superuser y grupo dentro de admin

--- proyecto_django/App/admin.py
+++ proyecto_django/App/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from App.models import socios, actividades, sucursales
+
+admin.site.register(socios)
+admin.site.register(actividades)
+admin.site.register(sucursales)
